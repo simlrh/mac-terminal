2efa880914fce23cb4cd6ddab70d26dd0e05940d
Steve Le Roy Harris <steven.leroy.harris@techwillsaveus.com>
2021-03-24 17:29:40 +0000

    Port Geoff's terminal to pico

--- fonts/font.c
+++ fonts/font.c
@@ -26,6 +26,11 @@ static int16_t find_glyph_index(const struct bitmap_font *font,
 
 const uint8_t *find_glyph(const struct bitmap_font *font,
                           uint16_t codepoint) {
+  // TODO: move null character in erus font
+  if (codepoint == 0) {
+    codepoint = 32;
+  }
+
   int16_t index = find_glyph_index(font, codepoint);
 
   if (index == -1)
